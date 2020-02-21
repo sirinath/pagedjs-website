40bb65227812448c52fb3d56709b2816c6b2221c
julientaq <julientaq@gmail.com>
2020-02-22 00:11:55 +0100

    rename themes

--- themes/pagedjs/assets/css/postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    plugins: [
-      require('postcss-import')(),
-      // require('postcss-url')({ url: 'copy', useHash: true }),
-      require('postcss-nested')(), 
-    ]
-  };
-  
