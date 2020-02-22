098bc65dff8aa329b7c7bbad4c4d65355c222661
julientaq <julientaq@gmail.com>
2020-02-22 02:22:11 +0100

    add bookish

---
+++ themes/bookish/assets/js/scripts.js
@@ -0,0 +1,37 @@
+
+// document.querySelector('footer').addEventListener("click", function() {
+//     this.classList.toggle("show");
+// })
+
+// console.log(document.querySelector('#toggleMenu'));
+
+// document.querySelector('#toggleMenu').addEventListener("click",  function() { document.querySelector("aside.menu").classList.toggle("leftHide")});
+
+if (document.querySelector('.toc .title')) {
+document.querySelector('.toc .title').addEventListener("click", function() {
+    document.querySelector('.toc').classList.toggle("show");
+})
+}
+
+document.querySelector(".menu-call").addEventListener("click", function() {document.querySelector(".menu").classList.toggle("hide")});
+
+
+if (document.querySelector('#print')) {
+document.querySelector('#print').addEventListener("click", print);
+}
+
+function print() {
+  window.PagedPolyfill.preview();
+}
+
+// if (document.querySelectorAll("#TableOfContents")) {
+// document.querySelectorAll("#TableOfContents").forEach(toc => {
+//   toc.classList.add("table-of-contents");
+//   toc.id = "";
+//   linkPrepend = toc.previousElementSibling.querySelector("a").getAttribute("href");
+//   toc.querySelectorAll("a").forEach(item => {
+//       let link = item.getAttribute("href");
+//       item.setAttribute("href", linkPrepend + link) 
+//   })
+// })
+// }
